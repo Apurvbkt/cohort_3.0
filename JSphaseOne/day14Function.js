c340ead241a053fe90894f645b570078cb048b0c
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2026-06-26 07:52:37 +0530

    pish function

---
+++ JSphaseOne/day14Function.js
@@ -0,0 +1,28 @@
+// 1. Funtion Declaration 
+function funName(){
+    // console.log("I am function declaration");
+    
+}
+funName()
+
+
+// 2. function Expression
+let funName2 = function(){
+    // console.log('i am function Expression')
+}
+funName2()
+
+
+// 3. Arrow function
+let funName3 = ()=>{
+    // console.log("I am arrow funtion");
+    
+}
+funName3()
+
+
+// 4. Anonymous Funtion
+setTimeout(function(){
+    // console.log("I am annonymous Funtion");
+    
+}, 1000)
